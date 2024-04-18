246a44ad8bd7b1185bf3631310b621d834d63b62
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-04-18 15:20:12 +0500

    User Module Added to the code

---
+++ src/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.model';
+
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    async createUser(@Body() user: User): Promise<User> {
+        return this.userService.createUser(user);
+    }
+
+    @Get()
+    async getAllUsers(): Promise<User[]> {
+        return this.userService.getAllUsers();
+    }
+
+    @Put(':id')
+    async updateUser(@Param('id') id: string, @Body() updateUserDto: Partial<User>): Promise<User> {
+        return this.userService.updateUser(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<User> {
+        return this.userService.deleteUser(id);
+    }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+// user.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.model';
+
+@Injectable()
+export class UserService {
+    constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
+
+    async createUser(user: User): Promise<User> {
+        const createdUser = new this.userModel(user);
+        return await createdUser.save();
+    }
+
+    async findUserByEmail(email: string): Promise<User | null> {
+        return this.userModel.findOne({ email }).exec();
+    }
+
+    async updateUser(id: string, updateUserDto: Partial<User>): Promise<User> {
+        return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+    }
+
+    async deleteUser(id: string): Promise<User> {
+        return this.userModel.findByIdAndDelete(id).exec();
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return this.userModel.find().exec();
+    }
+}
