53f95604e5126fa649cb319d001a25d0ed757b77
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-04-23 17:52:30 +0500

    Updated Code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { ForgetPasswordModule } from './forget-password/forget-password.module';
-import { UpdatePasswordModule } from './update-password/update-password.module';
+// import { ForgetPasswordModule } from './forget-password/forget-password.module';
+// import { UpdatePasswordModule } from './update-password/update-password.module';
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb://localhost:27017/nestjs_IPTV'),
         UserModule,
-        ForgetPasswordModule,
-        UpdatePasswordModule
+        // ForgetPasswordModule,
+        // UpdatePasswordModule
     ],
     controllers: [],
     providers: [],
