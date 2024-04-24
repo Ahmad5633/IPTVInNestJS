93290ca38b6257c09e23f4e922f85d89defac7d7
Ahmad Hameed <ahmadhameed5633@gmail.com>
2024-04-24 15:19:37 +0500

    Updated Code

---
+++ src/auth/facebook.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-facebook';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor() {
+    super({
+      clientID: '1888437871591715',
+      clientSecret: '89a1927abffb26ffeac3f77f67826c7d',
+      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any, done: any) {
+    const user = {
+      facebookId: profile.id,
+      displayName: profile.displayName,
+      email: profile.emails[0].value,
+      photoUrl: profile.photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
